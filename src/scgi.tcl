1538072f6b62cfac64452bb9b7e368b2b831e63a
非是非 <noyesno.net@gmail.com>
2015-09-11 15:11:31 +0800

    refactor scgi.tcl

--- src/scgi.tcl
+++ src/scgi.tcl
@@ -1,10 +1,35 @@
-
+#!/usr/bin/env tclsh
+# vim:set syntax=tcl: #
 
 #======================================================================#
 # SCGI: A Simple Common Gateway Interface alternative                  #
 #======================================================================#
 
 namespace eval scgi {
+  variable config [dict create]
+
+  proc @ {args} { }
+
+  proc config {key args} {
+    variable config
+
+    if [llength $args] {
+      dict set config $key {*}$args
+      return
+    }
+
+    set value ""
+
+    catch {
+      set value [dict get $config $key]
+    }
+
+    return $value 
+  }
+
+  proc debug {data} {
+    puts "debug: $data"
+  }
 
   proc print {sock data} {
     puts -nonewline $sock $data
@@ -18,11 +43,15 @@ namespace eval scgi {
     return [format "%s:%s," [string length $data] $data]
   }
 
-  proc accept {sock application} {
+  proc accept {sock args} {
     #-- upvar $headvar header
     #-- upvar $bodyvar body
 
+    fconfigure $sock -translation binary 
+
+    fconfigure $sock -blocking    0
     set buffer [read $sock 1024]
+    fconfigure $sock -blocking 1
     set idx [string first ":" $buffer]
 
     set header_length [string range $buffer 0 $idx-1]
@@ -35,11 +64,14 @@ namespace eval scgi {
 
     incr idx
     set header [dict create]
+    #debug $buffer 
     foreach {name value} [split [string range $buffer $idx $idx+$header_length] "\0"] {
       if {$name eq ","} break
 
       dict set header $name $value
     }
+
+    # debug $header
 
     incr idx $header_length
     incr idx
@@ -54,7 +86,13 @@ namespace eval scgi {
 
     set body $buffer
 
-    $application $header $body
+    set respond [config respond]
+
+    if {$respond ne ""} {
+      $respond $sock $header $body
+    } else {
+      source [config SCRIPT_FILENAME] 
+    }
 
     close $sock
 
@@ -79,7 +117,7 @@ namespace eval scgi {
     return
   }
 
-  proc response {sock header body} {
+  @ proc respond {sock header body} {
 
     dict foreach {name value} $header {
       println $sock "$name: $value"
@@ -94,7 +132,37 @@ namespace eval scgi {
 
 }
 
-return
+proc scgi::listen {port} {
+
+  if {[config "respond"] ne ""} {
+    # ...
+  } elseif {[info command respond] ne ""} {
+    config "respond" [namespace which respond]
+  } else {
+    config "respond" ""
+  }
+
+  puts "respond = [config respond]"
+
+  socket -server [list [namespace which accept]] $port 
+
+  return
+}
+
+
+if {[file normalize $::argv0] ne [file normalize [info script]]} {
+  return
+}
+
+
+lassign $argv script port
+
+scgi::config SCRIPT_FILENAME $script 
+source $script
+
+scgi::listen $port
+
+vwait scgi::forever
 
 #=================================================================================#
 # Notes                                                                           #
@@ -106,3 +174,4 @@ return
   * Standard CGI environment variables should be provided as SCGI headers.
 
   * close the connection after response
+
